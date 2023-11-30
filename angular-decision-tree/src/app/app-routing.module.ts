f4fcf0eee312575437b686bb4fa6919c5f529132
Silva <octavio.silva@vetta.digital>
2023-11-30 00:14:37 -0300

    feat: Configure routes

--- angular-decision-tree/src/app/app-routing.module.ts
+++ angular-decision-tree/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component1Component } from './component-1/component-1.component';
+import { Component2Component } from './component-2/component-2.component';
+import { Component3Component } from './component-3/component-3.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+	{ path: '1', component: Component1Component },
+	{ path: '2', component: Component2Component },
+	{
+		path: '3',
+		component: Component3Component,
+	},
+	{ path: '', redirectTo: '/1', pathMatch: 'full' },
+	{ path: '**', component: Component1Component },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
