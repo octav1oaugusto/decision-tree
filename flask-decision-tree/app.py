9b9ae03f977ae9a740eb30507da360d357b894db
Silva <octavio.silva@vetta.digital>
2023-12-11 12:10:27 -0300

    refactor: Solve CORS error

--- flask-decision-tree/app.py
+++ flask-decision-tree/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from utils import get_tree_data
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app)
 
 # Main route "/api"
 @app.route('/api', methods=['GET'])
@@ -8,6 +11,7 @@ def get_root():
     return 'This is the main route (/api).'
 
 # Route for GET method "/api/tree"
+@cross_origin(supports_credentials=True)
 @app.route('/api/tree', methods=['GET'])
 def get_tree():
     tree_data = get_tree_data()
