0d618300adef60f156032491f63f99a5fc7b7589
Silva <octavio.silva@vetta.digital>
2023-12-12 22:31:16 -0300

    feat: Add tree component

--- angular-decision-tree/src/app/component-2/component-2.component.ts
+++ angular-decision-tree/src/app/component-2/component-2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TreeNode } from '../models/tree.model';
+import { TreeNode } from 'primeng/api';
 import { DecisionTreeService } from '../providers/decision-tree.service';
 
 @Component({
@@ -12,15 +12,39 @@ export class Component2Component implements OnInit {
 
   public decisionTreeService: DecisionTreeService;
   treeData: TreeNode = {};
+  files!: TreeNode[];
 
   ngOnInit() {
     this.getTreeData();
+    // this.nodeService.getFiles().then((data) => (this.files = data));
   }
 
   getTreeData() {
     this.treeService.getTree().subscribe((data) => {
       this.treeData = data;
+      this.files = data;
       console.log('Tree Data:', this.treeData);
     });
+  }
+
+  expandAll() {
+    this.files.forEach((node) => {
+      this.expandRecursive(node, true);
+    });
+  }
+
+  collapseAll() {
+    this.files.forEach((node) => {
+      this.expandRecursive(node, false);
+    });
+  }
+
+  private expandRecursive(node: TreeNode, isExpand: boolean) {
+    node.expanded = isExpand;
+    if (node.children) {
+      node.children.forEach((childNode) => {
+        this.expandRecursive(childNode, isExpand);
+      });
+    }
   }
 }
